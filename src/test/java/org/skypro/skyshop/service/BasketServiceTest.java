ab8565a12c1e43bfcbabfad637b14896a41410a1
ikaterina <subsall@mail.ru>
2025-09-12 19:54:09 +0300

    SearchServiceTest added

---
+++ src/test/java/org/skypro/skyshop/service/BasketServiceTest.java
@@ -0,0 +1,17 @@
+package org.skypro.skyshop.service;
+
+import org.junit.jupiter.api.Test;
+
+public class BasketServiceTest {
+
+    @Test
+    public void givenBasket_whenAddingNotExistingElement_thenThrowException() {
+
+//        Добавление несуществующего товара в корзину приводит к выбросу исключения.
+//                Добавление существующего товара вызывает метод addProduct у мока ProductBasket.
+//                Метод getUserBacket возвращает пустую корзину, если ProductBasket пуст.
+//                Метод getUserBasket возвращает подходящую корзину, если в ProductBasket есть товары.
+
+    }
+
+}
---
+++ src/test/java/org/skypro/skyshop/service/SearchServiceTest.java
@@ -0,0 +1,79 @@
+package org.skypro.skyshop.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.skypro.skyshop.model.product.Product;
+import org.skypro.skyshop.model.product.SimpleProduct;
+import org.skypro.skyshop.model.search.SearchResult;
+import org.skypro.skyshop.model.search.Searchable;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class SearchServiceTest {
+
+    @Mock
+    private StorageService storageService;
+
+    @InjectMocks
+    private SearchService searchService;
+
+    @Test
+    public void givenEmptyStorage_whenSearch_thenReturnEmptyList() {
+        Mockito.when(storageService.getAllElements()).thenReturn(Collections.emptyMap());
+
+        List<SearchResult> result = searchService.search("мяч");
+
+        verify(storageService, Mockito.times(1)).getAllElements();
+
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"мяч", "Мяч"})
+    public void givenNotEmptyStorage_whenSearchingElementExists_thenReturnListOfElements(String stringParam) {
+        Map<UUID, Searchable> mapElements = fillTestedService();
+
+        Mockito.when(storageService.getAllElements()).thenReturn(mapElements);
+
+        List<SearchResult> result = searchService.search(stringParam);
+
+        Assertions.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void givenNotEmptyStorage_whenSearchingElementDoesNotExist_thenReturnEmptyList() {
+        Map<UUID, Searchable> mapElements = fillTestedService();
+
+        Mockito.when(storageService.getAllElements()).thenReturn(mapElements);
+
+        List<SearchResult> result = searchService.search("ball");
+
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    private Map<UUID, Searchable> fillTestedService() {
+        Map<UUID, Searchable> mapElements = new HashMap<>();
+        UUID id = UUID.randomUUID();
+        Product p1 = new SimpleProduct(id, "Мяч футзальный (Мяч для футбола в зале)", 500);
+        mapElements.put(id, p1);
+        id = UUID.randomUUID();
+        Product p22 = new SimpleProduct(id, "Мяч баскетбольный (размер 5)", 350);
+        mapElements.put(id, p22);
+
+        return mapElements;
+    }
+}
